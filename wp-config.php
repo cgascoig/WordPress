ac5f2574480ffbe5b4f1af634541b20b3a6b09c3
Chris Gascoigne <cgascoig@cisco.com>
2018-04-19 23:43:02 +1000

    update wp-config

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', '%DB_USER%' );
 define( 'DB_PASSWORD', '%DB_PASSWORD%' );
 
 /** MySQL hostname */
-define( 'DB_HOST', '%CliqrTier_mysql_IP%' );
+define( 'DB_HOST', '%DB_TIER_IP%' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
